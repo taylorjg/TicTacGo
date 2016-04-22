a7690ae27b24195b8514f1150888aebbb4eb4098
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-04-22 20:39:17 +0100

    Tweaks to imports and readme

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ with the following differences:
 
 * The frontend will use [Cycle.js](http://cycle.js.org/) instead of just JavaScript/jQuery.
 * The backend will be written in Go instead of node/Express.
+
+## Links
+
+* [Cycle.js](http://cycle.js.org/)
+* [Browserify, Babelify and ES6](http://egorsmirnov.me/2015/05/25/browserify-babelify-and-es6.html)
--- client/script.js
+++ client/script.js
@@ -1,16 +1,16 @@
 import Rx from "rx";
 import Cycle from "@cycle/core";
-import CycleDOM from "@cycle/dom"; 
+import {makeDOMDriver, h1} from "@cycle/dom"; 
 
 function main() {
     return {
         DOM: Rx.Observable.interval(1000)
-            .map(i => CycleDOM.h1(`${i} seconds elapsed`))
+            .map(i => h1(`${i} seconds elapsed`))
     };
 }
 
 const drivers = {
-    DOM: CycleDOM.makeDOMDriver("#app")
+    DOM: makeDOMDriver("#app")
 };
 
 Cycle.run(main, drivers);
