2d792af23eefe911597b95a94239ea77a696ff9d
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-04-22 23:28:17 +0100

    Added markup for the page

--- client/script.js
+++ client/script.js
@@ -4,8 +4,58 @@ import {makeDOMDriver, h1, hJSX} from "@cycle/dom";
 
 function main() {
     return {
-        DOM: Rx.Observable.interval(1000)
-            .map(i => <h1>{i} seconds elapsed</h1>)
+        DOM: Rx.Observable.of(
+    <div className="container">
+        <div className="row">
+            <div className="col-md-offset-4 col-md-4">
+                <h1>Tic-Tac-Toe</h1>
+                <table id="board">
+                    <tbody>
+                        <tr className="thickBottom">
+                            <td id="cell00" className="thickRight"></td>
+                            <td id="cell01" className="thickRight"></td>
+                            <td id="cell02"></td>
+                        </tr>
+                        <tr className="thickBottom">
+                            <td id="cell10" className="thickRight"></td>
+                            <td id="cell11" className="thickRight"></td>
+                            <td id="cell12"></td>
+                        </tr>
+                        <tr>
+                            <td id="cell20" className="thickRight"></td>
+                            <td id="cell21" className="thickRight"></td>
+                            <td id="cell22"></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div className="row" id="radioButtons">
+            <div className="col-md-offset-3 col-md-6">
+                <label for="noughtsRadio" className="radio-inline">
+                    <input type="radio" name="player1Piece" id="noughtsRadio" value="noughts">O</input>
+                </label>
+                <label for="crossesRadio" className="radio-inline">
+                    <input type="radio" name="player1Piece" id="crossesRadio" value="crosses">X</input>
+                </label>
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-md-offset-3 col-md-6">
+                <div className="alert alert-info">
+                    <span id="messageArea"></span>
+                    <img id="spinner" src="spinner.gif" alt="spinner" />
+                </div>
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-md-offset-3 col-md-6">
+                <button id="startBtn" className="btn btn-sm btn-primary" type="button">Start</button>
+                <button id="resetBtn" className="btn btn-sm btn-danger" type="button">Reset</button>
+            </div>
+        </div>
+    </div>
+        )
     };
 }
 
