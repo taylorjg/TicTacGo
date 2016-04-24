373503c9114909ee07352a1391d94a59f5ccdfc3
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-04-24 13:00:31 +0100

    Some simplifications in board component

--- client/board.js
+++ client/board.js
@@ -3,7 +3,7 @@ import {hJSX} from "@cycle/dom";
 
 const NOUGHT = "O"; 
 const CROSS = "X";
-const EMPTY = "-";
+const EMPTY = " ";
 const INITIAL_BOARD = EMPTY.repeat(9); 
 
 const IDS_TO_CELL_INDICES = {
@@ -18,17 +18,12 @@ const IDS_TO_CELL_INDICES = {
     "cell22": 8
 };
 
-const COMPUTER_MOVE_URL = "/api/computerMove"; 
+const COMPUTER_MOVE_URL = "/api/computerMove";
 
-function cellDisplayValue(state, index) {
-    var piece = state.board[index];
-    switch (piece) {
-        case NOUGHT:
-        case CROSS:
-            return piece; 
-        default:
-            return "";
-    } 
+function setCharAt(s, ch, index) {
+    const chs = s.split("");
+    chs[index] = ch;
+    return chs.join("");
 } 
 
 function intent(sources) {
@@ -55,11 +50,8 @@ function model(actions) {
             if (state.board[index] !== EMPTY) {
                 return state;
             }
-            const chars = state.board.split("");
-            chars[index] = state.player1Piece;
-            const updatedBoard = chars.join("");
             const updatedState = {
-                board: updatedBoard,
+                board: setCharAt(state.board, CROSS, index),
                 player1Piece: state.player1Piece,
                 player2Piece: state.player2Piece
             };
@@ -81,19 +73,19 @@ function view(state$) {
         <table id="board">
             <tbody>
                 <tr className="thickBottom">
-                    <td id="cell00" className="cell thickRight">{cellDisplayValue(state, 0)}</td>
-                    <td id="cell01" className="cell thickRight">{cellDisplayValue(state, 1)}</td>
-                    <td id="cell02" className="cell">{cellDisplayValue(state, 2)}</td>
+                    <td id="cell00" className="cell thickRight">{state.board[0]}</td>
+                    <td id="cell01" className="cell thickRight">{state.board[1]}</td>
+                    <td id="cell02" className="cell">{state.board[2]}</td>
                 </tr>
                 <tr className="thickBottom">
-                    <td id="cell10" className="cell thickRight">{cellDisplayValue(state, 3)}</td>
-                    <td id="cell11" className="cell thickRight">{cellDisplayValue(state, 4)}</td>
-                    <td id="cell12" className="cell">{cellDisplayValue(state, 5)}</td>
+                    <td id="cell10" className="cell thickRight">{state.board[3]}</td>
+                    <td id="cell11" className="cell thickRight">{state.board[4]}</td>
+                    <td id="cell12" className="cell">{state.board[5]}</td>
                 </tr>
                 <tr>
-                    <td id="cell20" className="cell thickRight">{cellDisplayValue(state, 6)}</td>
-                    <td id="cell21" className="cell thickRight">{cellDisplayValue(state, 7)}</td>
-                    <td id="cell22" className="cell">{cellDisplayValue(state, 8)}</td>
+                    <td id="cell20" className="cell thickRight">{state.board[6]}</td>
+                    <td id="cell21" className="cell thickRight">{state.board[7]}</td>
+                    <td id="cell22" className="cell">{state.board[8]}</td>
                 </tr>
             </tbody>
         </table>);
@@ -106,8 +98,8 @@ function makeComputerMoveRequest(state) {
         method: "POST",
         query: {
             board: state.board,
-            player1Piece: CROSS,
-            player2Piece: NOUGHT
+            player1Piece: state.player1Piece,
+            player2Piece: state.player2Piece
         }
     };
 }
