08181293fe77169bf750cd9743336859494f93a2
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-04-26 20:17:17 +0100

    Added basic board component

--- client/TicTacToe.js
+++ client/TicTacToe.js
@@ -1,25 +1,71 @@
 import {Observable, Subject} from "rx";
 import {hJSX} from "@cycle/dom";
-// import Board from "./board";
+import Board from "./board";
 import Buttons from "./buttons";
 
+const NOUGHT = "O"; 
+const CROSS = "X";
+const EMPTY = " ";
+const INITIAL_BOARD = EMPTY.repeat(9); 
+const COMPUTER_MOVE_URL = "/api/computerMove";
+
+function setCharAt(s, ch, index) {
+    const chs = s.split("");
+    chs[index] = ch;
+    return chs.join("");
+} 
+
+function makeComputerMoveRequest(state) {
+    return {
+        url: COMPUTER_MOVE_URL,
+        method: "POST",
+        send: {
+            board: state.board,
+            player1Piece: state.humanPiece,
+            player2Piece: state.computerPiece
+        },
+        category: "computerMove"
+    };
+}
+
 function model(actions) {
-    // chosenCell$
-    // newGame$
-    // HTTP (response$$)
-    // => state$
-    const state$ = Observable.just({
-        isGameOver: true
-    }).delay(1000); // TIMING ISSUE !!!
+    
+    function seedState() {
+        return {
+            board: INITIAL_BOARD,
+            humanPiece: CROSS,
+            computerPiece: NOUGHT,
+            isHumanMove: true,
+            // isGameOver: false,
+            isGameOver: true,
+            winningPlayer: null,
+            winningLine: null
+        };
+    }
+
+    const newGame$ = actions.newGame$.map(_ => _ => seedState());  
+    
+    const transform$ = Observable.merge(newGame$);
+    
+    const state$ = transform$
+        .startWith(seedState())
+        .scan((state, transform) => {
+            console.log("state", state);
+            const newState = transform(state);
+            console.log("newState", newState);
+            return newState;
+        })
+        .delay(50); // INITIALISATION TIMING ISSUE !!!
+        
     return state$;
 }
 
 function TicTacToe(sources) {
     const proxyState$ = new Subject();
-    // const board = Board(sources, proxyState$);
+    const board = Board(sources, proxyState$);
     const buttons = Buttons(sources, proxyState$);
     const actions = {
-        // chosenCell$: board.chosenCell$,
+        chosenCell$: board.chosenCell$,
         newGame$: buttons.newGame$,
         request$: new Subject(),
         response$$: sources.HTTP
@@ -29,11 +75,11 @@ function TicTacToe(sources) {
     state$.subscribe(proxyState$);
     
     return {
-        // DOM: Observable.combineLatest(buttons.DOM, (buttonsVTree) =>
-        //     <div>
-        //         {buttonsVTree}
-        //     </div>),
-        DOM: buttons.DOM,
+        DOM: Observable.combineLatest(board.DOM, buttons.DOM, (boardVTree, buttonsVTree) =>
+            <div>
+                {boardVTree}
+                {buttonsVTree}
+            </div>),
         HTTP: actions.request$
     };
 }
--- client/board.js
+++ client/board.js
@@ -1,11 +1,6 @@
 import {Observable, Subject} from "rx";
 import {hJSX} from "@cycle/dom";
 
-const NOUGHT = "O"; 
-const CROSS = "X";
-const EMPTY = " ";
-const INITIAL_BOARD = EMPTY.repeat(9); 
-
 const IDS_TO_CELL_INDICES = {
     "cell00": 0,
     "cell01": 1,
@@ -18,101 +13,87 @@ const IDS_TO_CELL_INDICES = {
     "cell22": 8
 };
 
-const COMPUTER_MOVE_URL = "/api/computerMove";
+// function intent(sources) {
+//     const actions = {
+//         cellSelected$: sources.DOM.select(".cell").events("click")
+//             .map(ev => ev.target.id)
+//             .map(id => IDS_TO_CELL_INDICES[id]),
+//         newGame$: sources.DOM.select(".newGame").events("click"),
+//         request$: new Subject(),
+//         response$$: sources.HTTP
+//     };
+//     return actions;
+// }
 
-function setCharAt(s, ch, index) {
-    const chs = s.split("");
-    chs[index] = ch;
-    return chs.join("");
-} 
+// function model(actions) {
+    
+//     function seedState() {
+//         return {
+//             board: INITIAL_BOARD,
+//             humanPiece: CROSS,
+//             computerPiece: NOUGHT,
+//             isHumanMove: true,
+//             isGameOver: false,
+//             winningPlayer: null,
+//             winningLine: null
+//         };
+//     }
+    
+//     const humanMove$ = actions.cellSelected$.map(index =>
+//         state => {
+//             if (state.isGameOver || !state.isHumanMove || state.board[index] !== EMPTY) {
+//                 return state;
+//             }
+//             const updatedState = {
+//                 board: setCharAt(state.board, state.humanPiece, index),
+//                 humanPiece: state.humanPiece,
+//                 computerPiece: state.computerPiece,
+//                 isHumanMove: false,
+//                 isGameOver: false,
+//                 winningPlayer: null,
+//                 winningLine: null
+//             };
+//             const request = makeComputerMoveRequest(updatedState);
+//             actions.request$.onNext(request);
+//             return updatedState;
+//         });
+        
+//     const computerMove$ = actions.response$$
+//         .filter(response$ => response$.request.category === "computerMove")
+//         .mergeAll()
+//         .map(response =>
+//             state => {
+//                 const updatedState = {
+//                     board: response.body.board,
+//                     humanPiece: state.humanPiece,
+//                     computerPiece: state.computerPiece,
+//                     isHumanMove: true,
+//                     isGameOver: response.body.gameOver,
+//                     winningPlayer: response.body.winningPlayer || null,
+//                     winningLine: response.body.winningLine || null
+//                 };
+//                 return updatedState;
+//             });
 
-function makeComputerMoveRequest(state) {
-    return {
-        url: COMPUTER_MOVE_URL,
-        method: "POST",
-        send: {
-            board: state.board,
-            player1Piece: state.humanPiece,
-            player2Piece: state.computerPiece
-        },
-        category: "computerMove"
-    };
-}
+//     const newGame$ = actions.newGame$.map(_ => _ => seedState());  
+    
+//     const transform$ = Observable.merge(
+//         humanMove$,
+//         computerMove$,
+//         newGame$);
+    
+//     const state$ = transform$
+//         .startWith(seedState())
+//         .scan((state, transform) => transform(state));
+        
+//     return state$;
+// }
 
 function intent(sources) {
     const actions = {
-        cellSelected$: sources.DOM.select(".cell").events("click")
-            .map(ev => ev.target.id)
-            .map(id => IDS_TO_CELL_INDICES[id]),
-        newGame$: sources.DOM.select(".newGame").events("click"),
-        request$: new Subject(),
-        response$$: sources.HTTP
+        chosenCell$: sources.DOM.select(".cell").events("click")
     };
     return actions;
-}
-
-function model(actions) {
-    
-    function seedState() {
-        return {
-            board: INITIAL_BOARD,
-            humanPiece: CROSS,
-            computerPiece: NOUGHT,
-            isHumanMove: true,
-            isGameOver: false,
-            winningPlayer: null,
-            winningLine: null
-        };
-    }
-    
-    const humanMove$ = actions.cellSelected$.map(index =>
-        state => {
-            if (state.isGameOver || !state.isHumanMove || state.board[index] !== EMPTY) {
-                return state;
-            }
-            const updatedState = {
-                board: setCharAt(state.board, state.humanPiece, index),
-                humanPiece: state.humanPiece,
-                computerPiece: state.computerPiece,
-                isHumanMove: false,
-                isGameOver: false,
-                winningPlayer: null,
-                winningLine: null
-            };
-            const request = makeComputerMoveRequest(updatedState);
-            actions.request$.onNext(request);
-            return updatedState;
-        });
-        
-    const computerMove$ = actions.response$$
-        .filter(response$ => response$.request.category === "computerMove")
-        .mergeAll()
-        .map(response =>
-            state => {
-                const updatedState = {
-                    board: response.body.board,
-                    humanPiece: state.humanPiece,
-                    computerPiece: state.computerPiece,
-                    isHumanMove: true,
-                    isGameOver: response.body.gameOver,
-                    winningPlayer: response.body.winningPlayer || null,
-                    winningLine: response.body.winningLine || null
-                };
-                return updatedState;
-            });
-
-    const newGame$ = actions.newGame$.map(_ => _ => seedState());  
-    
-    const transform$ = Observable.merge(
-        humanMove$,
-        computerMove$,
-        newGame$);
-    
-    const state$ = transform$
-        .startWith(seedState())
-        .scan((state, transform) => transform(state));
-        
-    return state$;
 }
 
 function renderCell(state, id) {
@@ -130,55 +111,52 @@ function renderCell(state, id) {
     return vtree$;
 }
 
-function renderButtonRow(state) {
-    const newGameButton = state.isGameOver
-        ? <button type="button" className="newGame btn btn-sm btn-primary">New Game</button>
-        : null;
-    const vtree$ =
-        <div className="row">
-            <div className="col-md-offset-4 col-md-4">
-                {newGameButton}
-            </div>
-        </div>;
-    return vtree$;
-}
+// function renderButtonRow(state) {
+//     const newGameButton = state.isGameOver
+//         ? <button type="button" className="newGame btn btn-sm btn-primary">New Game</button>
+//         : null;
+//     const vtree$ =
+//         <div className="row">
+//             <div className="col-md-offset-4 col-md-4">
+//                 {newGameButton}
+//             </div>
+//         </div>;
+//     return vtree$;
+// }
 
 function view(state$) {
     const vtree$ = state$.map(state =>
-        <div>
-            <div className="row">
-                <div className="col-md-offset-4 col-md-4">
-                    <table id="board">
-                        <tbody>
-                            <tr className="thickBottom">
-                                {renderCell(state, "cell00")}
-                                {renderCell(state, "cell01")}
-                                {renderCell(state, "cell02")}
-                            </tr>
-                            <tr className="thickBottom">
-                                {renderCell(state, "cell10")}
-                                {renderCell(state, "cell11")}
-                                {renderCell(state, "cell12")}
-                            </tr>
-                            <tr>
-                                {renderCell(state, "cell20")}
-                                {renderCell(state, "cell21")}
-                                {renderCell(state, "cell22")}
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+        <div className="row">
+            <div className="col-md-offset-4 col-md-4">
+                <table id="board">
+                    <tbody>
+                        <tr className="thickBottom">
+                            {renderCell(state, "cell00")}
+                            {renderCell(state, "cell01")}
+                            {renderCell(state, "cell02")}
+                        </tr>
+                        <tr className="thickBottom">
+                            {renderCell(state, "cell10")}
+                            {renderCell(state, "cell11")}
+                            {renderCell(state, "cell12")}
+                        </tr>
+                        <tr>
+                            {renderCell(state, "cell20")}
+                            {renderCell(state, "cell21")}
+                            {renderCell(state, "cell22")}
+                        </tr>
+                    </tbody>
+                </table>
             </div>
-            {renderButtonRow(state)}
         </div>);
     return vtree$;
 } 
 
-function Board(sources) {
+function Board(sources, state$) {
     const actions = intent(sources);
     return {
-        DOM: view(model(actions)),
-        HTTP: actions.request$
+        DOM: view(state$),
+        chosenCell$: actions.chosenCell$
     };
 }
 
